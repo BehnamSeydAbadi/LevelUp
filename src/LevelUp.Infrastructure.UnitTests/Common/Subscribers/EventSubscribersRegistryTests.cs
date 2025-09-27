553d2ef43a5c8ec99a2d627e793a3571f95f6f82
Behnam SeydAbadi <behnamsa96@outlook.com>
2025-09-26 21:22:52 -0700

    test: Pass "There are more than one event subscriber, When they get registered, Then it is done successfully"

--- src/LevelUp.Infrastructure.UnitTests/Common/Subscribers/EventSubscribersRegistryTests.cs
+++ src/LevelUp.Infrastructure.UnitTests/Common/Subscribers/EventSubscribersRegistryTests.cs
@@ -21,4 +21,33 @@ public class EventSubscribersRegistryTests
         eventSubscribers.Should().HaveCount(1);
         eventSubscribers[0].Equals(dummyDomainEventSubscriber).Should().BeTrue();
     }
+
+    [Fact(DisplayName =
+        "There are more than one event subscriber, When they get registered, Then it is done successfully")]
+    public void There_Are_More_Than_One_Event_Subscriber_When_They_Get_Registered_Then_It_Is_Done_Successfully()
+    {
+        var eventSubscribersRegistry = new EventSubscribersRegistry();
+
+        DummyDomainEventSubscriber[] domainEventSubscribers =
+        [
+            new(isUnique: true),
+            new(isUnique: true),
+            new(isUnique: true)
+        ];
+
+
+        foreach (var domainEventSubscriber in domainEventSubscribers)
+        {
+            eventSubscribersRegistry.Register(typeof(DummyDomainEvent), domainEventSubscriber);
+        }
+
+
+        var eventSubscribers = eventSubscribersRegistry.Get(typeof(DummyDomainEvent));
+
+        eventSubscribers.Should().HaveCount(domainEventSubscribers.Length);
+        
+        eventSubscribers
+            .GroupBy(es => (es as DummyDomainEventSubscriber)!.Id)
+            .Any(g => g.Count() > 1).Should().BeFalse();
+    }
 }
--- src/LevelUp.Infrastructure.UnitTests/Common/Subscribers/TestDoubles/DummyDomainEventSubscriber.cs
+++ src/LevelUp.Infrastructure.UnitTests/Common/Subscribers/TestDoubles/DummyDomainEventSubscriber.cs
@@ -5,12 +5,19 @@ namespace LevelUp.Infrastructure.UnitTests.Common.Subscribers.TestDoubles;
 
 public class DummyDomainEventSubscriber : IEventSubscriber
 {
+    public Guid Id { get; private set; } = Guid.Empty;
+
+    public DummyDomainEventSubscriber(bool isUnique = false)
+    {
+        if (isUnique) Id = Guid.NewGuid();
+    }
+
     public Task HandleAsync(IDomainEvent domainEvent)
     {
         return Task.CompletedTask;
     }
 
-    public override string ToString() => nameof(DummyDomainEventSubscriber);
+    public override string ToString() => $"{nameof(DummyDomainEventSubscriber)}_{Id}";
 
     public override bool Equals(object? obj)
     {
